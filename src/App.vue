d874ed8c38f2bf26f857cbc78866c4d2b88bceda
vsereda <vladimir45822@gmail.com>
2022-10-13 19:01:15 +0300

    17:40 V-On

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div>
-      <button>Like</button>
-      <button>Dislike</button>
+      <button @click="addLike">Like</button>
+      <button @click="addDisLike">Dislike</button>
     </div>
     <div>
       Количество лайков <strong>{{ likes }}</strong>
@@ -17,9 +17,17 @@ export default {
   data() {
     return {
       likes: 0,
-      dislikes: 5,
+      dislikes: 0,
     }
-  }
+  },
+  methods: {
+    addLike(){
+      this.likes += 1;
+    },
+    addDisLike(){
+      this.dislikes += 1;
+    },
+  },
 }
 </script>
 
