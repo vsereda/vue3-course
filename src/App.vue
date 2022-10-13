2ba80946247dface66736a726f4b4d468d62ade8
vsereda <vladimir45822@gmail.com>
2022-10-13 19:45:41 +0300

    25:35 new post creation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <div>
-
-    </div>
+  <div class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input class="input" type="text" placeholder="Название">
+      <input class="input" type="text" placeholder="Описание">
+      <button class="btn">Создать</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название:</strong> {{ post.title }}</div>
       <div><strong>Описание:</strong> {{ post.description }}</div>
@@ -16,29 +19,52 @@ export default {
   data() {
     return {
       posts: [
-        { id: 1, title: 'JavaScript', description: 'Описание JavaScript' },
-        { id: 2, title: 'PHP', description: 'Описание PHP' },
-        { id: 3, title: 'MySql', description: 'Описание MySql' },
-        { id: 4, title: 'Python', description: 'Описание Python' },
+        {id: 1, title: 'JavaScript', description: 'Описание JavaScript'},
+        {id: 2, title: 'PHP', description: 'Описание PHP'},
+        {id: 3, title: 'MySql', description: 'Описание MySql'},
+        {id: 4, title: 'Python', description: 'Описание Python'},
       ]
     }
   },
-  methods: {
-
-  },
+  methods: {},
 }
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  .post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-  }
+.app {
+  padding: 20px;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
 </style>
