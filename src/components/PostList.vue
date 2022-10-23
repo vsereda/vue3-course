fbcaca93df386cb9e9ff1b4d6282aeac81bb373e
vsereda <vladimir45822@gmail.com>
2022-10-23 16:20:06 +0300

    01:42:20 Transition group animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
-    <post-item
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    ></post-item>
+    <transition-group name="post-list">
+      <post-item
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      ></post-item>
+    </transition-group>
   </div>
   <h3 style="color: red;" v-else>
     Список постов пуст
@@ -15,6 +17,7 @@
 
 <script>
 import PostItem from "@/components/PostItem";
+
 export default {
   name: "PostList",
   components: {PostItem},
@@ -28,5 +31,23 @@ export default {
 </script>
 
 <style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
 
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.post-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
