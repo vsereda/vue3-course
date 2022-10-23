212cdbc059aec3c8c7772144fe14d02e8460e3b5
vsereda <vladimir45822@gmail.com>
2022-10-23 16:10:29 +0300

    01:39:08 Watch and computed

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
       </post-form>
     </my-dialog>
     <post-list
-        :posts="posts"
+        :posts="sortedPosts"
         @remove="removePost"
         v-if="!isPostsLoading"
     >
@@ -77,7 +77,13 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-  }
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    },
+  },
+  watch: {},
 }
 </script>
 
